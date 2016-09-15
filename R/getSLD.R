f83528b9e3c5bf2f10e217970c0284b67efc08ef
colin millar <colinpmillar@gmail.com>
2016-09-15 20:42:47 +0200

    added simple getSLD function

---
+++ R/getSLD.R
@@ -0,0 +1,30 @@
+#' Get Stock List Data
+#'
+#' Get haul data such as position, depth, sampling method, etc.
+#'
+#' @return A data frame.
+#'
+#' \code{\link{icesSLD-package}} gives an overview of the package.
+#'
+#' @examples
+#' sldata <- getSLD()
+#' str(sldata)
+#'
+#' @export
+
+getSLD <- function() {
+  # checks?
+
+  url <- "http://stocklist.ices.dk/services/odata4/StockListDWs4"
+
+  # read and parse jsonlite from API
+  out <- parseSLD(url)
+
+  # return
+  out
+}
+
+
+
+
+
